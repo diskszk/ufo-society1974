2fb2752fe4fa9de2481508cfbdecddffc6fb463f
diskszk <ibalucky23@gmail.com>
2023-09-15 11:18:14 +0900

    feat: AlbumFormのテストを書く

---
+++ packages/admin/src/partials/AlbumForm/AlbumForm.spec.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+import { AlbumForm } from "./AlbumForm";
+import { createMockAlbum } from "@ufo-society1974/factories";
+import { Wrapper } from "../../test-utils";
+
+const mockOnSubmit = jest.fn();
+
+jest.mock("../../lib/storages.ts", () => ({
+  uploadImage: (_file: never, _filename: never) => {
+    return { downloadURL: "" };
+  },
+}));
+
+test("新規作成の場合、入力欄は空である", () => {
+  render(
+    <Wrapper>
+      <AlbumForm isApproved={true} onSubmit={mockOnSubmit} />
+    </Wrapper>
+  );
+  expect(screen.getByRole("textbox", { name: "アルバムタイトル" })).toHaveValue(
+    ""
+  );
+
+  // eslint-disable-next-line testing-library/prefer-presence-queries
+  expect(screen.getByRole("img").getAttribute("src")).toBeFalsy();
+  expect(
+    screen.getByRole("textbox", { name: "公開日(YYYY-MM-DD)" })
+  ).toHaveValue("");
+
+  expect(screen.getByRole("button", { name: "保存する" })).toBeDisabled();
+});
+
+test("編集の場合、既存のアルバム情報を表示する", () => {
+  const testData = createMockAlbum("01", { image: "hello.png" });
+
+  render(
+    <Wrapper>
+      <AlbumForm
+        isApproved={true}
+        onSubmit={mockOnSubmit}
+        currentValues={{ ...testData, imageFile: testData.image }}
+      />
+    </Wrapper>
+  );
+  expect(screen.getByRole("textbox", { name: "アルバムタイトル" })).toHaveValue(
+    "テストアルバムタイトル01"
+  );
+  expect(screen.getByRole("img").getAttribute("src")).toBeTruthy();
+  expect(
+    screen.getByRole("textbox", { name: "公開日(YYYY-MM-DD)" })
+  ).toHaveValue("1995-02-03");
+
+  expect(screen.getByRole("button", { name: "保存する" })).toBeDisabled();
+});
